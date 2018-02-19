ff2701d467b08bb888205973fa91ba2400a01962
ngonhi <35695991+ngonhi@users.noreply.github.com>
2018-02-18 22:55:49 -0600

    Update Cooperator.java

--- src/Cooperator.java
+++ src/Cooperator.java
@@ -1,32 +1,31 @@
-
 public class Cooperator extends Organism{
 	// Fields
 	private double cooperProb;
 	private String type;
-	
+
 	// Constructor 
 	public Cooperator() {
 		super();
 		cooperProb = 1.0;
 		type = "Cooperate";
 	}
-	
+
 	// Methods
 	public String getType() {
 		return type;
 	} //getType
-	
+
 	public Organism reproduce() {
 		this.energy -= 10;
 		return new Cooperator();
 	} //reproduce
-	
+
 	public double getCooperationProbability() {
 		return cooperProb;
 	} //getCooperationProbability
-	
+
 	public boolean cooperates() {
 		return true;
 	} //cooperates
-	
+
 } // class Cooperator
